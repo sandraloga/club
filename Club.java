a441f681a00e399d360c19b604a46b5b882ba8b6
Sandra <72sandraloga@gmail.com>
2016-02-15 12:00:36 +0100

    Creado metodo que devuelve un entero con los socios que se han dado de alta un mes introducido por parametro

--- Club.java
+++ Club.java
@@ -8,23 +8,24 @@ import java.util.ArrayList;
 public class Club
 {
     // Define any necessary fields here ...
-    private ArrayList<Membership> member;
+    private ArrayList<Membership> members;
+    private Membership member3;
    
     /**
      * Constructor for objects of class Club
      */
     public Club()
     {
-       member = new ArrayList<Membership>(); 
+       members = new ArrayList<Membership>(); 
     }
 
     /**
      * Add a new member to the club's list of members.
      * @param member The member object to be added.
      */
-    public void join(Membership miembros)
+    public void join(Membership miembro)
     {
-      member.add(miembros);  
+      members.add(miembro);  
     }
 
     /**
@@ -33,6 +34,31 @@ public class Club
      */
     public int numberOfMembers()
     {
-        return member.size();
+        return members.size();
     }
+    /** Calcula el numero de socios que se dieron de alta en un mes determinado. 
+     * El año no nos importa. En caso de que el parametro contenga un valor 
+     * no valido se muestra por pantalla el error.
+     * @param month El mes en el que estamos interesados
+     * @return El numero de socios que se dieron de alta dicho mes
+     * */
+     public int joinedMonth(int month)
+     {
+         int contadorMes = 0;
+         while (month > 1 || month > 12)
+         {
+             System.out.println("El parametro introducido no es valido (de 1 a 12)");
+         }
+         
+         for ( Membership member : members)
+         {
+             if (member.getMonth() == month)
+             {
+              contadorMes = contadorMes + 1;   
+             }
+         }
+         return contadorMes;
+         
+     }
+     
 }
